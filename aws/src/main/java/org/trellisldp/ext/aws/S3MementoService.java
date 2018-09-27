1747c420812e45f76163e116460d5a791f4f7776
Aaron Coburn <acoburn@apache.org>
2018-09-27 09:27:03 -0400

    Add memento listing implementation

--- aws/src/main/java/org/trellisldp/ext/aws/S3MementoService.java
+++ aws/src/main/java/org/trellisldp/ext/aws/S3MementoService.java
@@ -15,21 +15,32 @@ package org.trellisldp.ext.aws;
 
 import static com.amazonaws.services.s3.AmazonS3ClientBuilder.defaultClient;
 import static java.io.File.createTempFile;
+import static java.lang.Long.parseLong;
+import static java.lang.String.join;
+import static java.time.Instant.now;
+import static java.time.Instant.ofEpochSecond;
+import static java.util.Collections.unmodifiableList;
+import static java.util.Objects.nonNull;
 import static java.util.concurrent.CompletableFuture.runAsync;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
+import static org.apache.commons.lang3.Range.between;
 import static org.apache.jena.riot.Lang.NQUADS;
 import static org.slf4j.LoggerFactory.getLogger;
 import static org.trellisldp.api.RDFUtils.TRELLIS_DATA_PREFIX;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Stream;
@@ -79,13 +90,7 @@ public class S3MementoService implements MementoService {
     @Override
     public CompletableFuture<Void> put(final IRI identifier, final Instant time, final Stream<? extends Quad> data) {
         return runAsync(() -> {
-            final File file;
-            try {
-                file = createTempFile("trellis-memento-", ".nq");
-            } catch (final IOException ex) {
-                LOGGER.error("Error creating temporary file: {}", ex.getMessage());
-                throw new RuntimeTrellisException(ex);
-            }
+            final File file = getTempFile();
             try (final JenaDataset dataset = rdf.createDataset();
                     final OutputStream output = new FileOutputStream(file)) {
                 data.forEach(dataset::add);
@@ -96,17 +101,14 @@ public class S3MementoService implements MementoService {
                 LOGGER.error("Error closing dataset: {}", ex.getMessage());
                 throw new RuntimeTrellisException(ex);
             }
-            client.putObject(new PutObjectRequest(bucketName, getKey(identifier), file));
-            // TODO -- add version id
-                    //.withVersionId(getVersionId(time)));
+            client.putObject(new PutObjectRequest(bucketName, getKey(identifier, time), file));
         });
     }
 
     @Override
     public CompletableFuture<Resource> get(final IRI identifier, final Instant time) {
         return supplyAsync(() -> {
-            final GetObjectRequest req = new GetObjectRequest(bucketName, getKey(identifier))
-                .withVersionId(getVersionId(time));
+            final GetObjectRequest req = new GetObjectRequest(bucketName, getKey(identifier, time));
             return new S3Resource(client.getObject(req));
         });
     }
@@ -114,25 +116,61 @@ public class S3MementoService implements MementoService {
     @Override
     public CompletableFuture<List<Range<Instant>>> list(final IRI identifier) {
         return supplyAsync(() -> {
-            // TODO
-            // List the objects for this resource
-            // client.listVersions(...)
-            // new ListVersionsRequest(bucket, key...)
-            return null;
+            final List<Instant> versions = new ArrayList<>();
+            final ListObjectsRequest req = new ListObjectsRequest().withBucketName(bucketName)
+                .withPrefix(getKey(identifier, null)).withDelimiter("/");
+            ObjectListing objs = client.listObjects(req);
+            objs.getObjectSummaries().stream().map(S3ObjectSummary::getKey).map(this::getInstant)
+                .forEachOrdered(versions::add);
+            while (objs.isTruncated()) {
+                objs = client.listNextBatchOfObjects(objs);
+                objs.getObjectSummaries().stream().map(S3ObjectSummary::getKey).map(this::getInstant)
+                    .forEachOrdered(versions::add);
+            }
+
+            final List<Range<Instant>> mementos = new ArrayList<>();
+            Instant last = null;
+            for (final Instant time : versions) {
+                if (nonNull(last)) {
+                    mementos.add(between(last, time));
+                }
+                last = time;
+            }
+            if (nonNull(last)) {
+                mementos.add(between(last, now()));
+            }
+            return unmodifiableList(mementos);
         });
+    }
+
+    private Instant getInstant(final String key) {
+        final String[] parts = key.split("?version=", 2);
+        if (parts.length == 2) {
+            return ofEpochSecond(parseLong(parts[1]));
+        }
+        return null;
     }
 
     @Override
     public CompletableFuture<Void> delete(final IRI identifier, final Instant time) {
         return runAsync(() ->
-            client.deleteVersion(bucketName, getKey(identifier), getVersionId(time)));
+            client.deleteObject(bucketName, getKey(identifier, time)));
     }
 
-    private static String getVersionId(final Instant time) {
-        return Long.toString(time.toEpochMilli());
+    private static String getKey(final IRI identifier, final Instant time) {
+        if (nonNull(time)) {
+            return join("?version=", identifier.getIRIString().substring(TRELLIS_DATA_PREFIX.length()),
+                    Long.toString(time.getEpochSecond()));
+        }
+        return identifier.getIRIString().substring(TRELLIS_DATA_PREFIX.length()) + "?version=";
     }
 
-    private static String getKey(final IRI identifier) {
-        return identifier.getIRIString().substring(TRELLIS_DATA_PREFIX.length());
+    private static File getTempFile() {
+        try {
+            return createTempFile("trellis-memento-", ".nq");
+        } catch (final IOException ex) {
+            LOGGER.error("Error creating temporary file: {}", ex.getMessage());
+            throw new RuntimeTrellisException(ex);
+        }
     }
 }
